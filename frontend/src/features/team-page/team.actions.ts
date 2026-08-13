6a961ea4c99bf4f137d425b36d77bed527de2436
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-08-13 15:14:57 +1000

    feat: array of objects with information on members

---
+++ frontend/src/features/team-page/team.actions.ts
@@ -0,0 +1,33 @@
+export const teamMembers = [
+  {
+    name: 'Declan Thomas',
+    role: 'Project Manager (PM)',
+    blurb: 'Hi, My Name is Declan. I am 21 and currently studying a Bachelor of Information Technology (Professional). Currently work part-time as an IT MSP Technician, dealing with IT issues for multiple companies in my home region. My main hobbies outside of work and uni are gaming; I really enjoy playing Escape from Tarkov and playing it with my friends. I have recently started taking up fishing and have been enjoying it so far',
+    imageURL: '/team/declan.jpeg',
+  },
+  {
+    name: 'Abdifatah Hashi',
+    role: 'Business Analyst (BA)',
+    blurb:
+      'I’m Abdifatah, the business analyst for team 39. My role is to help define the requirements, clarify what needs to be built, and make sure designs match project requirements',
+    imageURL: '/team/abdifatah.jpeg',
+  },
+  {
+    name: 'Wyatt Truong',
+    role: 'User Experience (UX)',
+    blurb: 'My name is Wyatt Truong. I am 20 years old and studying a Bachelor of information technology (professional) and my interests include programming and web design. I am the UX designer for group 39 which allows me to design the look of the program my team are building.',
+    imageURL: '/team/wyatt.jpeg',
+  },
+  {
+    name: 'Ilya Kosobrukhov',
+    role: 'Developer (Dev)',
+    blurb: '',
+    imageURL: '',
+  },
+  {
+    name: 'Sameer Siddiqui',
+    role: 'Developer (Dev)',
+    blurb: 'My name is Sameer Siddiqui. I am 20 years old and I am studying Bachelor of Information Technology (Professional). My interests in IT include Cybersecurity, Networking and Cloud Computing. I am the developer of group 39 and my as developer allows me to get hands on and build the program which my team wants me to build.',
+    imageURL: '/team/sameer.jpg',
+  },
+]
