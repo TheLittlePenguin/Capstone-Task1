9aaef05718b032150239c38f35fc687779deb927
ilya-kos <s3948725@student.rmit.edu.au>
2026-08-16 00:45:13 +1000

    docs: minor changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,6 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
+    // SET IN .env !!!
     baseURL: process.env.TEST_BASE_URL!,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
